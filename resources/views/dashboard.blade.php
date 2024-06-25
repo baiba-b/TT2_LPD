b8d30cd0f0a2e0ed7daf75187cc5c330b5ae0274
baiba <baibina.berzina@gmail.com>
2024-06-26 00:23:22 +0300

    Added paths.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,18 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="\css\dashboardStylesheet.css">
+    <link rel="stylesheet" href="{{ asset('css/dashboardStylesheet.css') }}">
     <title>Dashboard</title>
 </head>
 <body>
-<!-- add routes in href, see welcome blade -->
 <ul>
-    <li><a class="active" href="{{url('/dashboard')}}">Home</a></li>
-    <li><a href="{{url('/Teams')}}">Teams</a></li> 
-    <li><a href="{{url('/Connections')}}">Connections</a></li>
-    <li><a href="{{url('/Projects')}}">Projects</a></li>
-    <li><a href="{{url('/Tasks')}}">Tasks</a></li>
-    <li><a href="{{url('/Timeline')}}">Timeline</a></li>
+    <li><a class="active" href="{{ route('dashboard') }}">Home</a></li>
+    <li><a href="{{ route('teams.index') }}">Teams</a></li> 
+    <li><a href="{{ route('connections.index') }}">Connections</a></li>
+    <li><a href="{{ route('projects.index') }}">Projects</a></li>
+    <li><a href="{{ route('tasks.index') }}">Tasks</a></li>
+    <li><a href="{{ route('timeline.index') }}">Timeline</a></li>
 </ul> 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,53 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\ProjectRoleController;
+use App\Http\Controllers\TaskRoleController;
+use App\Http\Controllers\ConnectionController;
+use App\Http\Controllers\CommunicationsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::redirect('/', '/homepage');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
+Route::redirect('/', '/hompeage');
 Route::get('/homepage', function () {
     return view('homepage');
 });
+
+// Dashboard route
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+// Resource routes
+Route::resource('users', UserController::class);
+Route::resource('tasks', TaskController::class);
+Route::resource('projects', ProjectController::class);
+Route::resource('notifications', NotificationController::class);
+Route::resource('project-roles', ProjectRoleController::class);
+Route::resource('task-roles', TaskRoleController::class);
+Route::resource('connections', ConnectionController::class);
+Route::resource('messages', CommunicationsController::class);
+
+// Custom routes for tasks
+Route::get('tasks/{task}/participants', [TaskController::class, 'showParticipants'])->name('tasks.participants');
+Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+
+// Custom routes for projects
+Route::get('projects/{project}/participants', [ProjectController::class, 'showParticipants'])->name('projects.participants');
+Route::post('projects/{project}/send-alert', [ProjectController::class, 'sendAlert'])->name('projects.sendAlert');
+
+// Custom routes for notifications
+Route::post('notifications/{notification}/mark-as-read', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
+
+// Custom routes for connections
+Route::post('connections/{connection}/accept', [ConnectionController::class, 'acceptRequest'])->name('connections.acceptRequest');
+Route::post('connections/{connection}/reject', [ConnectionController::class, 'rejectRequest'])->name('connections.rejectRequest');
+
+// Custom route for sending messages
+Route::post('messages/send', [CommunicationsController::class, 'sendMessage'])->name('messages.sendMessage');
