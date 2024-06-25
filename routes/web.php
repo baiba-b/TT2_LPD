99f448d321407e0e217b20359eb9eac94c92018e
baiba <baibina.berzina@gmail.com>
2024-06-25 21:46:40 +0300

    Made initial controllers. Added / path to home page. Started "dashboard.blade.php" view. Added an almost empty homepage view.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::redirect('/', '/homepage');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
