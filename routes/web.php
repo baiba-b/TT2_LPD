74d9e37d375083207248e78c5495f310d9494979
nuLabi <nulabi.exe@gmail.com>
2024-06-26 17:25:34 +0300

    Added our routes back

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\ProjectRoleController;
+use App\Http\Controllers\TaskRoleController;
+use App\Http\Controllers\ConnectionController;
+use App\Http\Controllers\CommunicationsController;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +23,37 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::redirect('/', '/homepage');
+
+Route::redirect('/home', '/homepage');
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+// Resource routes
+Route::resource('users', UserController::class);
+Route::resource('tasks', TaskController::class);
+Route::resource('projects', ProjectController::class);
+Route::resource('notifications', NotificationController::class);
+Route::resource('project-roles', ProjectRoleController::class);
+Route::resource('task-roles', TaskRoleController::class);
+Route::resource('connections', ConnectionController::class);
+Route::resource('messages', CommunicationsController::class);
+// Custom routes for tasks
+Route::get('tasks/{task}/participants', [TaskController::class, 'showParticipants'])->name('tasks.participants');
+Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+// Custom routes for projects
+Route::get('projects/{project}/participants', [ProjectController::class, 'showParticipants'])->name('projects.participants');
+Route::post('projects/{project}/send-alert', [ProjectController::class, 'sendAlert'])->name('projects.sendAlert');
+// Custom routes for notifications
+Route::post('notifications/{notification}/mark-as-read', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
+// Custom routes for connections
+Route::post('connections/{connection}/accept', [ConnectionController::class, 'acceptRequest'])->name('connections.acceptRequest');
+Route::post('connections/{connection}/reject', [ConnectionController::class, 'rejectRequest'])->name('connections.rejectRequest');
+// Custom route for sending messages
+Route::post('messages/send', [CommunicationsController::class, 'sendMessage'])->name('messages.sendMessage');
