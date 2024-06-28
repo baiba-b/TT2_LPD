497211086b6801379106c3c90b4d820e337f01e2
nuLabi <nulabi.exe@gmail.com>
2024-06-28 03:25:04 +0300

    Added dashboard navbar to connections view.

--- resources/views/connections.blade.php
+++ resources/views/connections.blade.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="\css\connectionsStylesheet.css">
+    <link rel="stylesheet" href="\css\navbarStylesheet.css">
     <title>Connections</title>
 </head>
 <body>
+<nav>
+        <ul>
+
+            <li><a href="{{ route('dashboard') }}">Home</a></li>
+            <li><a class="active" href="{{ route('connections.index') }}">Connections</a></li>
+            <li><a href="{{ route('projects.index') }}">Projects</a></li>
+            <li><a href="{{ route('tasks.index') }}">Tasks</a></li>
+            <li><a href="{{url('/Timeline')}}">Timeline</a></li>
+            <li>
+                <a href="{{ url('/profile') }}" id="profile-info">
+                    @if(Auth::user()->profile_picture)
+                    <img src="{{ asset('storage/' . Auth::user()->profile_picture) }}" alt="Profile Picture" class="exception" style="width: 35px; height: 35px;">
+                    @else
+                    <img src="{{ asset('default_profile_picture.png') }}" alt="Default Profile Picture" class="exception" style="width: 35px; height: 35px;">
+                    @endif
+                    <span class="profile-name">{{ Auth::user()->name }}</span>
+                </a>
+            </li>
+
+        </ul>
+    </nav>
 <div class="container">
         <div class="section">
             <h2>Mutual Connections</h2>
