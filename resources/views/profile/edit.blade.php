53a56390eb761e3f7d025a85b442bab1709691fc
baiba <baibina.berzina@gmail.com>
2024-06-28 04:36:25 +0300

    Fixed profile pic issue.

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,21 +4,11 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-
     <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
         <div class="max-w-xl">
-            <div class="mb-4">
-                <h3 class="text-lg font-medium text-gray-900">{{ __('Profile Picture') }}</h3>
-                @if(Auth::user()->profile_picture)
-                    <img src="public\default_profile_picture.png" alt="Profile Picture" class="rounded-full w-35px h-35px">
-                @else
-                    <img src="{{public\default_profile_picture.png}}" alt="Default Profile Picture" class="rounded-full w-35px h-35px">
-                @endif
-            </div>
             @include('profile.partials.update-profile-picture-form')
         </div>
     </div>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
